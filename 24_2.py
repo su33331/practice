2bc9b6d719c48721c8e47498b582f86b9ff3f795
su33331 <ks6500308@gmail.com>
2024-06-02 18:14:17 +0900

    [fix] コードレビューを反映

--- 24_2.py
+++ 24_2.py
@@ -13,55 +13,47 @@
 #3rd:4min
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        tail = dummyHead
+        dummy_head = ListNode()
+        tail = dummy_head
         carry = 0
 
-        while l1 != None or l2 != None or carry != 0:
+        while l1 or l2 or carry != 0:
             l1_val = l1.val if l1 else 0
             l2_val = l2.val if l2 else 0
             total = l1_val + l2_val + carry
-            next_val = total % 10
             carry = total // 10
 
-            next_node = ListNode(next_val)
+            next_node = ListNode(total % 10)
             tail.next = next_node
             tail = tail.next
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
-        l3 = dummyHead.next
-        return l3
+        return dummy_head.next
 
 ### 参考を見て書いた他方法
 #再帰を使う方法
+#dummy_headを使わない方法
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        l3 = dummyHead
         carry = 0
-
-        def add_two_num_helper(l1, l2, carry, l3):
+        def add_two_num_helper(l1, l2, carry):
             if l1 is None and l2 is None and carry == 0:
                 return None
-            else:
-                l1_val = l1.val if l1 else 0
-                l2_val = l2.val if l2 else 0
-                total = l1_val + l2_val + carry
-                next_val = total % 10
-                carry = total // 10
 
-                next_node = ListNode(next_val)
-                l3.next = next_node
-                l3 = l3.next
-                l1 = l1.next if l1 else None
-                l2 = l2.next if l2 else None
-                l3.next = add_two_num_helper(l1, l2, carry, l3)
-                return l3
-        l3 = add_two_num_helper(l1, l2, carry, l3)
-        result = dummyHead.next
-        return result
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            total = l1_val + l2_val + carry
+            carry = total // 10
+
+            new_node = ListNode(total % 10)
+            sum_node_head = new_node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            sum_node_head.next = add_two_num_helper(l1, l2, carry)
+            return sum_node_head
+        return add_two_num_helper(l1, l2, carry)
 
 '''
 参考にしたもの
